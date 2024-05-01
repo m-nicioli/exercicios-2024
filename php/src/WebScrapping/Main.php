c00feea57caa3718f88bd42b3293b388a9bf3463
Mariel Nicioli <132221995+m-nicioli@users.noreply.github.com>
2024-05-01 15:43:59 -0300

    Add files via upload

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,8 +2,11 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+// require_once __DIR__ . 'spout-3.3.0/src/Spout/Autoloader/autoload.php';
+
 /**
- * Runner for the Webscrapping exercice.
+ * Runner for the Webscrapping exercise.
  */
 class Main {
 
@@ -14,10 +17,47 @@ class Main {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+    $xpath = new \DOMXPath($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $query = "//div[@class='col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4']";
+    $trabalhos = $xpath->query($query);
+
+    // Verifica se foram encontrados elementos
+    if ($trabalhos->length > 0) {
+      $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Título');
+        $sheet->setCellValue('B1', 'Autores');
+        $sheet->setCellValue('C1', 'Apresentação');
+        $sheet->setCellValue('D1', 'Volume');
+
+        $row = 2;
+        // Loop através dos elementos encontrados
+        foreach ($trabalhos as $trabalho) {
+            // Extrai os dados dos elementos filhos
+            $titulo = $xpath->query(".//h4[@class='my-xs paper-title']", $trabalho)->item(0)->nodeValue;
+            // Consulta XPath para obter autores
+            $autores = $xpath->query(".//div[@class='authors']", $trabalho)->item(0)->nodeValue;
+            // Consulta XPath para obter apresentação
+            $presentation = $xpath->query(".//div[@class='tags mr-sm']", $trabalho)->item(1)->nodeValue;
+            // Consulta XPath para obter volume
+            $volume = $xpath->query(".//div[@class='volume-info']", $trabalho)->item(0)->nodeValue;
+            
+            // Adiciona os dados na planilha
+            $sheet->setCellValue('A' . $row, $titulo);
+            $sheet->setCellValue('B' . $row, $autores);
+            $sheet->setCellValue('C' . $row, $presentation);
+            $sheet->setCellValue('D' . $row, $volume);
+            
+            $row++;
+        }
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save(__DIR__ . '/../../assets/Planilha.xlsx');
+
+        echo "Planilha gerada e salva com sucesso!";
+    }
   }
-
 }
+
+Main::run();
